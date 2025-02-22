4ed4168d47ed7880d7b3d96bd38d178442c7b696
V <144168944+veronicavvf@users.noreply.github.com>
2025-02-22 17:56:32 -0300

    quadrados-perfeitos.asm

---
+++ submission/quadrados-perfeitos.asm
@@ -0,0 +1,37 @@
+@ /000                 
+INICIO   LV 0         
+         MM N        
+         MM QUAD     
+         LV 1        
+         MM SOMA    
+
+LOOP     LD QUAD     
+         MM ENDERECO
+         ST ENDERECO 
+
+         LD QUAD    
+         AD SOMA     
+         MM QUAD    
+
+         LD SOMA    
+         AD DOIS    
+         MM SOMA    
+
+         LD N       
+         AD UM      
+         MM N       
+
+         CP N, LIMITE 
+         JZ FIM      
+         JP LOOP     
+
+FIM      HM =0       
+
+@ /0F0               
+N        K =0        
+QUAD     K =0       
+SOMA     K =1       
+DOIS     K =2       
+UM       K =1       
+LIMITE   K =64      
+ENDERECO K =100      
