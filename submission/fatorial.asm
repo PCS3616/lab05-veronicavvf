9a4e7af409a8aeb1702111605ea48dd389500d74
V <144168944+veronicavvf@users.noreply.github.com>
2025-03-20 19:04:23 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,25 +1,24 @@
 
-SC INICIO
-HM /000
+START   SC /200 
+END     HM =0   
 
-VAL_1       K /0001  ; Constante com valor 1
-VAL_0       K /0000  ; Constante com valor 0
-RESP_1      K /0001 
-
-INICIO      K /010
-            LD /100  ; Carrega o valor de N (posição 0x100) no acumulador (AC)
-            ML RESP_1 
-            MM RESP_1 
-            LD /1OO
-            SB VAL_1 ; Subtrai 1 de N (AC = N - 1)
-            MM /100  ; Armazena o novo valor de N
-            JZ FIM   ; Se N for 0, termina o programa
-            JN FIM   ; Se N for negativo, termina o programa
-            JP INICIO 
-            
-
-
-FIM    K/030
-            LD RESP_1
-            MM /102
-            RS INICIO
+@ /100
+N       k =0    
+RES     k =1    
+AUX1    k =1   
+AUX2    k =1  
+ONE     k =1   
+ 
+@ /200
+JP START       
+LD N            
+JZ /212         
+MM AUX1         
+ML AUX2         
+MM AUX2         
+LD AUX1         
+SB ONE          
+JP /204         
+LD AUX2         
+MM RES          
+RS /200         
